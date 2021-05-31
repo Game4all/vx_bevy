4abf59696c2c9921e53b379c53aa6259e4eed24a
Lucas A <game4allyt@gmail.com>
2021-05-31 09:34:24 +0200

    Add better worldgen (still broken)

--- crates/vx_core/src/world/worldgen.rs
+++ crates/vx_core/src/world/worldgen.rs
@@ -15,12 +15,16 @@ pub trait TerrainGenerator {
 
 #[derive(Default)]
 pub struct NoiseTerrainGenerator {
-    seed: i32
+    seed: i32,
 }
 
 impl TerrainGenerator for NoiseTerrainGenerator {
+    fn set_seed(&mut self, seed: i32) {
+        self.seed = seed;
+    }
+
     fn generate(&self, chunk_pos: IVec2, data: &mut Array3x1<Voxel>) {
-        let noise = simdnoise::NoiseBuilder::fbm_2d_offset(
+        let heightmap = simdnoise::NoiseBuilder::fbm_2d_offset(
             (chunk_pos.x * CHUNK_WIDTH) as f32,
             CHUNK_WIDTH as usize,
             (chunk_pos.y * CHUNK_DEPTH) as f32,
@@ -32,54 +36,39 @@ impl TerrainGenerator for NoiseTerrainGenerator {
 
         for x in 0..CHUNK_WIDTH {
             for z in 0..CHUNK_DEPTH {
-                let height =
-                    noise.get((z * CHUNK_WIDTH + x) as usize).unwrap() * CHUNK_HEIGHT as f32 * 4.0;
+                let original_height = heightmap
+                    .get((z * CHUNK_WIDTH + x) as usize)
+                    .unwrap()
+                    .abs();
 
-                let block_height = (height.round() as i32).max(0).min(CHUNK_HEIGHT - 1);
+                let height = original_height * 8.0;
+                let block_height = (original_height * CHUNK_HEIGHT as f32) as i32;
+                let color = self.get_color_for_height(height);
 
-                data.fill_extent(
-                    &ExtentN::from_min_and_max(
-                        PointN([0; 3]),
-                        PointN([CHUNK_WIDTH, 0, CHUNK_DEPTH]),
-                    ),
-                    Voxel {
-                        attributes: [194, 178, 128, 255],
-                    },
-                );
-
-                if block_height == 0 {
-                    data.fill_extent(
-                        &ExtentN::from_min_and_max(
-                            PointN([0; 3]),
-                            PointN([CHUNK_WIDTH, 1, CHUNK_DEPTH]),
-                        ),
-                        Voxel {
-                            attributes: [0, 10, 128, 255],
-                        },
-                    );
-                }
-
-                if block_height > 8 {
-                    let extent = ExtentN::from_min_and_max(PointN([x, 0, z]), PointN([x, 8, z]));
-                    data.fill_extent(
-                        &extent,
-                        Voxel {
-                            attributes: [194, 178, 128, 255],
-                        },
-                    );
-                }
-
-                data.fill_extent(
-                    &ExtentN::from_min_and_max(PointN([x, 9, z]), PointN([x, block_height, z])),
-                    Voxel {
-                        attributes: [99, 146, 103, 255],
-                    },
-                );
+                let extent =
+                    ExtentN::from_min_and_max(PointN([x, 0, z]), PointN([x, block_height, z]));
+                data.fill_extent(&extent, Voxel { attributes: color })
             }
         }
     }
+}
 
-    fn set_seed(&mut self, seed: i32) {
-        self.seed = seed;
+impl NoiseTerrainGenerator {
+    fn get_color_for_height(&self, height: f32) -> [u8; 4] {
+        if height < 0.3 {
+            [74, 97, 210, 255]
+        } else if height < 0.4 {
+            [102, 133, 254, 255]
+        } else if height < 0.45 {
+            [96, 200, 102, 255]
+        } else if height < 0.65 {
+            [64, 152, 72, 255]
+        } else if height < 0.8 {
+            [122, 121, 87, 255]
+        } else if height < 0.9 {
+            [99, 99, 88, 255]
+        } else {
+            [255; 4]
+        }
     }
 }
