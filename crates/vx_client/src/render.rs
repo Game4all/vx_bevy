b7bd20f6bfa3374ec0c20a92633e9db5c2dda89d
Lucas A <game4allyt@gmail.com>
2021-05-31 19:38:44 +0200

    Add a bit of ambient lighting to the scene

--- crates/vx_client/src/render.rs
+++ crates/vx_client/src/render.rs
@@ -1,6 +1,7 @@
 use std::collections::VecDeque;
 
 use bevy::{
+    pbr::AmbientLight,
     prelude::*,
     reflect::TypeUuid,
     render::{
@@ -135,6 +136,11 @@ impl Plugin for WorldRenderPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_event::<ChunkMeshingEvent>()
             .init_resource::<VecDeque<ChunkMeshingEvent>>()
+            .insert_resource(ClearColor(Color::hex("87CEEB").unwrap()))
+            .insert_resource(AmbientLight {
+                brightness: 0.4,
+                ..Default::default()
+            })
             .add_startup_system(setup_render_resources.system())
             .add_system(attach_chunk_render_bundle.system())
             .add_system(handle_chunk_ready_events.system())
