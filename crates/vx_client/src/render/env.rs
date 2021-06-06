0131f1a928ac171487d84feafdaac61f3e809a53
Lucas A <game4allyt@gmail.com>
2021-06-06 13:18:38 +0200

    Add basic tod sunlight update

--- crates/vx_client/src/render/env.rs
+++ crates/vx_client/src/render/env.rs
@@ -1,18 +1,34 @@
 use bevy::prelude::*;
 
+const ILLUMINANCE_BASE: f32 = 4_000.0;
+
 //todo: have dynamic directional sunlight.
 fn setup_sun_lighting(mut commands: Commands) {
     commands
         .spawn()
-        .insert(DirectionalLight::new(Color::WHITE, 4_000.0, -Vec3::Y))
+        .insert(DirectionalLight::new(
+            Color::WHITE,
+            ILLUMINANCE_BASE,
+            -Vec3::Y,
+        ))
         .insert(Transform::default())
         .insert(GlobalTransform::default());
+}
+
+fn update_sun_lighting_intensity(mut query: Query<&mut DirectionalLight>, time: Res<Time>) {
+    if let Ok(mut light) = query.single_mut() {
+        let illuminance = (time.time_since_startup().as_secs_f32() * 0.1).cos() * ILLUMINANCE_BASE;
+        if illuminance > 0f32 {
+            light.illuminance = illuminance;
+        }
+    }
 }
 
 pub struct EnvLightingPlugin;
 
 impl Plugin for EnvLightingPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(setup_sun_lighting.system());
+        app.add_startup_system(setup_sun_lighting.system())
+            .add_system(update_sun_lighting_intensity.system());
     }
 }
