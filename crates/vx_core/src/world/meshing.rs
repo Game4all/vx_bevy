6dc007e96d2885db9aadce9b4e5a65a095dd80f8
Lucas A <game4allyt@gmail.com>
2021-06-19 11:45:09 +0200

    Make meshing multithreaded

--- crates/vx_core/src/world/meshing.rs
+++ crates/vx_core/src/world/meshing.rs
@@ -3,14 +3,18 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use bevy::{prelude::*, render::mesh::Indices};
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, pipeline::PrimitiveTopology},
+    tasks::ComputeTaskPool,
+};
 use building_blocks::{
     core::Extent3i,
     mesh::{greedy_quads, GreedyQuadsBuffer},
     storage::Get,
 };
 
-use crate::{config::GlobalConfig, utils::ChunkMeshBuilder};
+use crate::utils::ChunkMeshBuilder;
 
 use super::{chunk_extent, ChunkInfo, ChunkMeshInfo, ChunkReadyEvent, WorldChunkMap};
 
@@ -48,61 +52,73 @@ pub(crate) fn mesh_chunks(
     mut meshing_requests: ResMut<VecDeque<ChunkMeshingRequest>>,
     mut meshes: ResMut<Assets<Mesh>>,
     chunk_map: ResMut<WorldChunkMap>,
-    mut greedy_buffer: bevy::ecs::system::Local<ReusableGreedyQuadsBuffer>,
-    config: Res<GlobalConfig>,
+    task_pool: Res<ComputeTaskPool>,
 ) {
-    for _ in 0..(config.render_distance / 2) {
-        if let Some(meshing_event) = meshing_requests.pop_back() {
+    let mesh_results = task_pool.scope(|scope| {
+        for meshing_event in meshing_requests.drain(..) {
             if let Ok((chunk_info, mut mesh_info)) = chunks.get_mut(meshing_event.0) {
-                let chunk_data = chunk_map.get(&chunk_info.pos).unwrap();
-                let extent = padded_chunk_extent();
-
-                greedy_buffer.reset(extent);
-                greedy_quads(chunk_data, &extent, &mut greedy_buffer);
-
-                let mut chunk_mesh_builder = ChunkMeshBuilder::default();
-
-                for group in greedy_buffer.quad_groups.iter() {
-                    for quad in group.quads.iter() {
-                        chunk_mesh_builder.add_quad_to_mesh(
-                            &group.face,
-                            quad,
-                            &chunk_data.get(quad.minimum),
-                        );
-                    }
-                }
-
-                let ChunkMeshBuilder {
-                    positions,
-                    normals,
-                    indices,
-                    colors,
-                    uv,
-                    fluid_positions,
-                    fluid_normals,
-                    fluid_indices,
-                    fluid_colors,
-                    fluid_uv,
-                } = chunk_mesh_builder;
-
-                let terrain_mesh = meshes.get_mut(&mesh_info.chunk_mesh).unwrap();
-
-                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv);
-                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, colors);
-                terrain_mesh.set_indices(Some(Indices::U32(indices)));
-
-                let fluid_mesh = meshes.get_mut(&mesh_info.fluid_mesh).unwrap();
-
-                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, fluid_positions);
-                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, fluid_normals);
-                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, fluid_uv);
-                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, fluid_colors);
-                fluid_mesh.set_indices(Some(Indices::U32(fluid_indices)));
-
+                //mark that the mesh_info was updated
                 mesh_info.set_changed();
+
+                let chunk_data = chunk_map.get(&chunk_info.pos).unwrap();
+                scope.spawn(async move {
+                    let mut greedy_buffer = GreedyQuadsBuffer::new_with_y_up(padded_chunk_extent());
+                    let extent = padded_chunk_extent();
+
+                    greedy_buffer.reset(extent);
+                    greedy_quads(chunk_data, &extent, &mut greedy_buffer);
+
+                    let mut chunk_mesh_builder = ChunkMeshBuilder::default();
+
+                    for group in greedy_buffer.quad_groups.iter() {
+                        for quad in group.quads.iter() {
+                            chunk_mesh_builder.add_quad_to_mesh(
+                                &group.face,
+                                quad,
+                                &chunk_data.get(quad.minimum),
+                            );
+                        }
+                    }
+
+                    let ChunkMeshBuilder {
+                        positions,
+                        normals,
+                        indices,
+                        colors,
+                        uv,
+                        fluid_positions,
+                        fluid_normals,
+                        fluid_indices,
+                        fluid_colors,
+                        fluid_uv,
+                    } = chunk_mesh_builder;
+
+                    let mut terrain_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+                    terrain_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+                    terrain_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+                    terrain_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv);
+                    terrain_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+                    terrain_mesh.set_indices(Some(Indices::U32(indices)));
+
+                    let mut fluid_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+                    fluid_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, fluid_positions);
+                    fluid_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, fluid_normals);
+                    fluid_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, fluid_uv);
+                    fluid_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, fluid_colors);
+                    fluid_mesh.set_indices(Some(Indices::U32(fluid_indices)));
+
+                    (meshing_event.0, terrain_mesh, fluid_mesh)
+                });
             }
+        }
+    });
+
+    for (chunk, terrain_mesh, fluid_mesh) in mesh_results {
+        if let Ok((___, mesh_info)) = chunks.get_mut(chunk) {
+            *meshes.get_mut(&mesh_info.chunk_mesh).unwrap() = terrain_mesh;
+            *meshes.get_mut(&mesh_info.fluid_mesh).unwrap() = fluid_mesh;
         }
     }
 }
