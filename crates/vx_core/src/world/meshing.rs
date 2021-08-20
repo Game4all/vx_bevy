0c8abcfef38d171ab06a5af533989fa053c6264f
Lucas A <game4allyt@gmail.com>
2021-08-20 13:26:29 +0200

    Don't mesh empty chunks

--- crates/vx_core/src/world/meshing.rs
+++ crates/vx_core/src/world/meshing.rs
@@ -47,48 +47,52 @@ pub(crate) fn mesh_chunks(
                             greedy_buffer.reset(extent);
                             greedy_quads(chunk_data, &extent, &mut greedy_buffer);
 
-                            let mut chunk_mesh_builder = ChunkMeshBuilder::default();
+                            if greedy_buffer.num_quads() != 0 {
+                                let mut chunk_mesh_builder = ChunkMeshBuilder::default();
 
-                            for group in greedy_buffer.quad_groups.iter() {
-                                for quad in group.quads.iter() {
-                                    chunk_mesh_builder.add_quad_to_mesh(
-                                        &group.face,
-                                        quad,
-                                        &chunk_data.get(quad.minimum),
-                                    );
+                                for group in greedy_buffer.quad_groups.iter() {
+                                    for quad in group.quads.iter() {
+                                        chunk_mesh_builder.add_quad_to_mesh(
+                                            &group.face,
+                                            quad,
+                                            &chunk_data.get(quad.minimum),
+                                        );
+                                    }
                                 }
+
+                                let ChunkMeshBuilder {
+                                    positions,
+                                    normals,
+                                    indices,
+                                    colors,
+                                    uv,
+                                    fluid_positions,
+                                    fluid_normals,
+                                    fluid_indices,
+                                    fluid_colors,
+                                    fluid_uv,
+                                } = chunk_mesh_builder;
+
+                                let mut terrain_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+                                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+                                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+                                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv);
+                                terrain_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+                                terrain_mesh.set_indices(Some(Indices::U32(indices)));
+
+                                let mut fluid_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+                                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, fluid_positions);
+                                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, fluid_normals);
+                                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, fluid_uv);
+                                fluid_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, fluid_colors);
+                                fluid_mesh.set_indices(Some(Indices::U32(fluid_indices)));
+
+                                Some((meshing_event.0, terrain_mesh, fluid_mesh))
+                            } else {
+                                None
                             }
-
-                            let ChunkMeshBuilder {
-                                positions,
-                                normals,
-                                indices,
-                                colors,
-                                uv,
-                                fluid_positions,
-                                fluid_normals,
-                                fluid_indices,
-                                fluid_colors,
-                                fluid_uv,
-                            } = chunk_mesh_builder;
-
-                            let mut terrain_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-
-                            terrain_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-                            terrain_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-                            terrain_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uv);
-                            terrain_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, colors);
-                            terrain_mesh.set_indices(Some(Indices::U32(indices)));
-
-                            let mut fluid_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-
-                            fluid_mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, fluid_positions);
-                            fluid_mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, fluid_normals);
-                            fluid_mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, fluid_uv);
-                            fluid_mesh.set_attribute(Mesh::ATTRIBUTE_COLOR, fluid_colors);
-                            fluid_mesh.set_indices(Some(Indices::U32(fluid_indices)));
-
-                            (meshing_event.0, terrain_mesh, fluid_mesh)
                         });
                     }
                 }
@@ -100,12 +104,14 @@ pub(crate) fn mesh_chunks(
         }
     });
 
-    for (chunk, terrain_mesh, fluid_mesh) in mesh_results {
-        if let Ok((___, mesh_info, mut load_state)) = chunks.get_mut(chunk) {
-            *meshes.get_mut(&mesh_info.chunk_mesh).unwrap() = terrain_mesh;
-            *meshes.get_mut(&mesh_info.fluid_mesh).unwrap() = fluid_mesh;
-            if *load_state < ChunkLoadState::Idle {
-                *load_state = ChunkLoadState::Idle;
+    for meshing_result in mesh_results {
+        if let Some((chunk, terrain_mesh, fluid_mesh)) = meshing_result {
+            if let Ok((___, mesh_info, mut load_state)) = chunks.get_mut(chunk) {
+                *meshes.get_mut(&mesh_info.chunk_mesh).unwrap() = terrain_mesh;
+                *meshes.get_mut(&mesh_info.fluid_mesh).unwrap() = fluid_mesh;
+                if *load_state < ChunkLoadState::Idle {
+                    *load_state = ChunkLoadState::Idle;
+                }
             }
         }
     }
