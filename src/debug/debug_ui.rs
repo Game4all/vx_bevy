fd2ed1b4b6fe39c6297b17dad69912c5c1264f1f
Lucas A <game4allyt@gmail.com>
2022-02-03 14:47:17 +0100

    Allow toggling debug UI display

--- src/debug/debug_ui.rs
+++ src/debug/debug_ui.rs
@@ -1,6 +1,8 @@
 use bevy::{
     diagnostic::{Diagnostics, EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin},
-    prelude::{CoreStage, Plugin, Res, ResMut, SystemStage},
+    ecs::schedule::ShouldRun,
+    input::{keyboard::KeyboardInput, ElementState},
+    prelude::{CoreStage, EventReader, KeyCode, Plugin, Res, ResMut, SystemSet, SystemStage},
 };
 use bevy_egui::{
     egui::{self, Slider},
@@ -8,23 +10,6 @@ use bevy_egui::{
 };
 
 use crate::voxel::{storage::VoxelMap, ChunkLoadingRadius, ChunkShape, Voxel};
-
-pub struct DebugUIPlugins;
-
-impl Plugin for DebugUIPlugins {
-    fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_plugin(EguiPlugin)
-            .add_plugin(FrameTimeDiagnosticsPlugin)
-            .add_plugin(EntityCountDiagnosticsPlugin)
-            .add_stage_after(
-                CoreStage::PostUpdate,
-                "debug_ui_stage",
-                SystemStage::parallel()
-                    .with_system(display_debug_stats)
-                    .with_system(display_chunk_stats),
-            );
-    }
-}
 
 fn display_debug_stats(egui: ResMut<EguiContext>, diagnostics: Res<Diagnostics>) {
     egui::Window::new("performance stuff").show(egui.ctx(), |ui| {
@@ -61,3 +46,48 @@ fn display_chunk_stats(
         ui.separator();
     });
 }
+
+fn display_debug_ui_criteria(ui_state: Res<DebugUIState>) -> ShouldRun {
+    if ui_state.0 {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
+
+fn toggle_ui_display(mut inputs: EventReader<KeyboardInput>, mut ui_state: ResMut<DebugUIState>) {
+    for input in inputs.iter() {
+        match input.key_code {
+            Some(key_code) if key_code == KeyCode::F3 && input.state == ElementState::Pressed => {
+                ui_state.0 = !ui_state.0;
+            }
+            _ => {}
+        }
+    }
+}
+
+pub struct DebugUIPlugins;
+
+impl Plugin for DebugUIPlugins {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_plugin(EguiPlugin)
+            .add_plugin(FrameTimeDiagnosticsPlugin)
+            .add_plugin(EntityCountDiagnosticsPlugin)
+            .add_stage_after(
+                CoreStage::PostUpdate,
+                "debug_ui_stage",
+                SystemStage::parallel()
+                    .with_system(toggle_ui_display)
+                    .with_system_set(
+                        SystemSet::new()
+                            .with_system(display_debug_stats)
+                            .with_system(display_chunk_stats)
+                            .with_run_criteria(display_debug_ui_criteria),
+                    ),
+            )
+            .init_resource::<DebugUIState>();
+    }
+}
+
+#[derive(Default)]
+struct DebugUIState(bool);
