524c131778311508e0751b0d29f7fd1b00832802
Lucas A <game4allyt@gmail.com>
2022-02-04 19:26:16 +0100

    Merge terrain_gen systems

--- src/voxel/world/terrain.rs
+++ src/voxel/world/terrain.rs
@@ -1,11 +1,7 @@
 use bevy::{
-    prelude::{
-        Added, ParallelSystemDescriptorCoercion, Plugin, Query, Res, ResMut, StageLabel,
-        SystemStage,
-    },
+    prelude::{Added, Local, Plugin, Query, Res, ResMut, StageLabel, SystemStage},
     tasks::ComputeTaskPool,
 };
-use std::collections::VecDeque;
 
 use super::{
     chunks::{ChunkLoadingStage, DirtyChunks},
@@ -13,27 +9,25 @@ use super::{
 };
 use crate::voxel::storage::VoxelMap;
 
-//todo: move this in the gen_terrain system.
-fn queue_terrain_gen(chunks: Query<&Chunk, Added<Chunk>>, mut gen_queue: ResMut<TerrainGenQueue>) {
-    gen_queue.0.extend(chunks.iter().map(|chunk| chunk.0));
-}
-
 fn gen_terrain(
     mut chunk_data: ResMut<VoxelMap<Voxel, ChunkShape>>,
-    mut gen_queue: ResMut<TerrainGenQueue>,
+    mut gen_queue: Local<Vec<ChunkKey>>,
     mut dirty_chunks: ResMut<DirtyChunks>,
     task_pool: Res<ComputeTaskPool>,
     gen_budget: Res<WorldTerrainGenFrameBudget>,
+    chunks: Query<&Chunk, Added<Chunk>>,
 ) {
-    let drain_size = if gen_queue.0.len() < gen_budget.gen_per_frame {
-        gen_queue.0.len()
+    gen_queue.extend(chunks.iter().map(|chunk| chunk.0));
+
+    let drain_size = if gen_queue.len() < gen_budget.gen_per_frame {
+        gen_queue.len()
     } else {
         gen_budget.gen_per_frame
     };
+
     //do the terrain gen here
     let generated_terrain = task_pool.scope(|scope| {
         gen_queue
-            .0
             .drain(..drain_size)
             .map(|chunk_pos| (chunk_pos, chunk_data.remove(chunk_pos).unwrap()))
             .map(|(chunk_pos, mut buffer)| {
@@ -72,14 +66,8 @@ impl Plugin for VoxelWorldTerrainGenPlugin {
         app.add_stage_after(
             ChunkLoadingStage,
             TerrainGenStage,
-            SystemStage::parallel()
-                .with_system(queue_terrain_gen.label("queue_terrain_gen"))
-                .with_system(gen_terrain.after("queue_terrain_gen")),
+            SystemStage::single(gen_terrain),
         )
-        .init_resource::<TerrainGenQueue>()
         .insert_resource(WorldTerrainGenFrameBudget { gen_per_frame: 16 });
     }
 }
-
-#[derive(Default)]
-struct TerrainGenQueue(VecDeque<ChunkKey>);
