f626001bf6c78dd703a657efb0dc07e64fbb1d90
Lucas A <game4allyt@gmail.com>
2022-07-05 14:41:40 +0200

    delete & rename stuff

--- src/voxel/terraingen/mod.rs
+++ src/voxel/terraingen/mod.rs
@@ -1,21 +1,15 @@
 use float_ord::FloatOrd;
-use std::{collections::BTreeMap, sync::RwLock};
+use std::sync::RwLock;
 
-use bevy::{math::Vec3Swizzles, prelude::Plugin};
+use bevy::prelude::Plugin;
 use once_cell::sync::Lazy;
 
 use self::{
     common::terrain_generate_world_bottom_border,
-    generators::{FlatBiomeTerrainGenerator, HeightmapBiomeTerrainGenerator},
     noise::{generate_heightmap_data, Heightmap},
 };
 
-use super::{
-    material::VoxelMaterial,
-    materials::{Grass, Sand, Water},
-    storage::VoxelBuffer,
-    ChunkKey, ChunkShape, Voxel, CHUNK_LENGTH_U,
-};
+use super::{storage::VoxelBuffer, ChunkKey, ChunkShape, Voxel, CHUNK_LENGTH_U};
 
 mod generators;
 
@@ -28,10 +22,10 @@ pub mod common;
 // Terrain generator singleton.
 pub static TERRAIN_GENERATOR: Lazy<RwLock<TerrainGenerator>> = Lazy::new(|| Default::default());
 
-/// A trait representing terrain generation for a specific biome type.
+/// A trait representing a terrain generator for a biome.
 pub trait BiomeTerrainGenerator: 'static + Sync + Send {
-    /// Generate the general terrain shape for a chunk.
-    fn generate_terrain(
+    /// Carve the terrain using the materials for the biome.
+    fn carve_terrain(
         &self,
         chunk_key: ChunkKey,
         heightmap: Heightmap<f32, CHUNK_LENGTH_U, CHUNK_LENGTH_U>,
@@ -55,28 +49,28 @@ impl<T: BiomeTerrainGenerator> IntoBoxedTerrainGenerator for T {
 
 #[derive(Default)]
 pub struct TerrainGenerator {
-    biomes_map: BTreeMap<FloatOrd<f32>, Box<dyn BiomeTerrainGenerator>>,
+    // biomes_map: BTreeMap<FloatOrd<f32>, Box<dyn BiomeTerrainGenerator>>,
 }
 
 impl TerrainGenerator {
-    pub fn register_biome(&mut self, biome: Box<dyn BiomeTerrainGenerator>) -> &mut Self {
-        self.biomes_map.insert(biome.biome_temp_humidity(), biome);
-        self
-    }
+    // pub fn register_biome(&mut self, biome: Box<dyn BiomeTerrainGenerator>) -> &mut Self {
+    //     self.biomes_map.insert(biome.biome_temp_humidity(), biome);
+    //     self
+    // }
 
     // returns the biome with the closest temp / humidity
-    fn biome_at(&self, chunk_key: ChunkKey) -> &Box<dyn BiomeTerrainGenerator> {
-        const BIOME_INVSCALE: f32 = 0.001;
+    // fn biome_at(&self, chunk_key: ChunkKey) -> &Box<dyn BiomeTerrainGenerator> {
+    //     const BIOME_INVSCALE: f32 = 0.001;
 
-        let coords =
-            noise::voronoi(chunk_key.location().xzy().truncate().as_vec2() * BIOME_INVSCALE);
-        let p = FloatOrd(noise::rand2to1i(coords));
+    //     let coords =
+    //         noise::voronoi(chunk_key.location().xzy().truncate().as_vec2() * BIOME_INVSCALE);
+    //     let p = FloatOrd(noise::rand2to1i(coords));
 
-        self.biomes_map
-            .range(..=p)
-            .last()
-            .map_or(self.biomes_map.first_key_value().unwrap().1, |x| x.1)
-    }
+    //     self.biomes_map
+    //         .range(..=p)
+    //         .last()
+    //         .map_or(self.biomes_map.first_key_value().unwrap().1, |x| x.1)
+    // }
 
     pub fn generate(&self, chunk_key: ChunkKey, buffer: &mut VoxelBuffer<Voxel, ChunkShape>) {
         // let biome = self.biome_at(chunk_key);
@@ -97,21 +91,5 @@ impl TerrainGenerator {
 pub struct TerrainGeneratorPlugin;
 
 impl Plugin for TerrainGeneratorPlugin {
-    fn build(&self, _: &mut bevy::prelude::App) {
-        TERRAIN_GENERATOR
-            .write()
-            .expect("Failed to acquire terrain generator singleton.")
-            .register_biome(
-                HeightmapBiomeTerrainGenerator::new(Grass::into_voxel(), 0.0f32)
-                    .into_boxed_generator(),
-            )
-            .register_biome(
-                FlatBiomeTerrainGenerator::new(Water::into_voxel(), 1.419f32)
-                    .into_boxed_generator(),
-            )
-            .register_biome(
-                HeightmapBiomeTerrainGenerator::new(Sand::into_voxel(), 0.8f32)
-                    .into_boxed_generator(),
-            );
-    }
+    fn build(&self, _: &mut bevy::prelude::App) {}
 }
